ac039c4af7fd28e811ef19276917fb191f64285d
Evelyn Chen <evelync93@gmail.com>
2019-11-19 15:39:32 -0500

    add recipe link text box

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: [],
+      recipe: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ recipe: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    //extract ingredients API here
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Recipe Link:
+            <input
+              type="text"
+              value={this.state.recipe}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
